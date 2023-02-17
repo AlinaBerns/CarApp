98610b8b1dac9b870f2ce23f97967ee2ffbea77d
AlinaBerns <123542234+AlinaBerns@users.noreply.github.com>
2023-02-17 16:42:03 +0100

    today

--- src/be/intecbrussel/Car.java
+++ src/be/intecbrussel/Car.java
@@ -1,2 +1,84 @@
-package be.intecbrussel;public class Car {
+package be.intecbrussel;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Car {
+
+    protected int maxSnelheid, huidigeSnelheid, paadenkrachtis, max_rijden, motor;
+
+    protected String kleur, user_kleur;
+
+    protected List<Integer> random;
+
+    {
+        maxSnelheid = 120;
+        paadenkrachtis = 150;
+        max_rijden = 100;
+        motor = 100;
+        kleur = "wit";
+        random = new Random().ints(1, 100, 200).boxed().toList();
+    }
+
+    Car() {
+    }
+
+    Car(String kleur) {
+        this.kleur = kleur;
+
+    }
+
+    Car(int max_rijden, String kleur) {
+        this.max_rijden = max_rijden;
+        this.kleur = kleur;
+    }
+
+    Car(int motor, int maxSnelheid, String kleur) {
+        this.motor = motor;
+        this.maxSnelheid = maxSnelheid;
+        this.kleur = kleur;
+    }
+
+    int snelHeidplus() {
+
+        for (huidigeSnelheid = 0; huidigeSnelheid < maxSnelheid + 5; huidigeSnelheid += 5) {
+            System.out.println(huidigeSnelheid);
+
+
+        }
+
+        return 0;
+    }
+
+    int snelHeidmin() {
+
+        for (huidigeSnelheid = maxSnelheid; huidigeSnelheid > 0; huidigeSnelheid -= 5) {
+            System.out.println(huidigeSnelheid);
+        }
+        return 0;
+    }
+
+    String user_kleur_in() {
+
+        System.out.println("Voel jouw kleur: ");
+        Scanner in = new Scanner(System.in);
+        user_kleur = in.nextLine();
+
+        this.kleur = user_kleur;
+
+        return user_kleur;
+
+    }
+
+    List<Integer> randomNum() {
+        System.out.println("repair cost is: " + random);
+
+        return random;
+    }
+
 }
+
+
+
+
--- src/be/intecbrussel/CarApp.java
+++ src/be/intecbrussel/CarApp.java
@@ -1,2 +1,32 @@
-package be.intecbrussel;public class CarApp {
+package be.intecbrussel;
+import java.util.Arrays;
+import java.util.List;
+
+public class CarApp {
+    public static void main(String[] args) {
+        Car car1 = new Car("paars");
+        Car car2 = new Car(70, "groen");
+        Car car3 = new Car(200, 160,"geel");
+
+
+
+        System.out.println("***CAR 1***"+"\n"+"max snelheid: "+car1.maxSnelheid+ "\n" + "paardenkrachtis: "+ car1.paadenkrachtis +"\n"+ "max rijden: "+car1.max_rijden+"\n"+"motor: "+car1.motor+"\n"+"kleur: "+car1.kleur+"\n\n");
+        System.out.println("***CAR 2***"+"\n"+"max snelheid: "+car1.maxSnelheid+ "\n" + "paardenkrachtis: "+ car1.paadenkrachtis +"\n"+ "max rijden: "+car2.max_rijden+"\n"+"motor: "+car1.motor+"\n"+"kleur: "+car2.kleur+"\n\n");
+        System.out.println("***CAR 3***"+"\n"+"max snelheid: "+car3.maxSnelheid+ "\n" + "paardenkrachtis: "+ car1.paadenkrachtis +"\n"+ "max rijden: "+car1.max_rijden+"\n"+"motor: "+car3.motor+"\n"+"kleur: "+car3.kleur+"\n\n");
+
+        System.out.println("De huidige snelheid CAR 1: ");
+        int car1_pl = car1.snelHeidplus();
+
+        System.out.println("De huidige snelheid CAR 3: ");
+        int car3_min = car3.snelHeidmin();
+
+        String userKleur = car1.user_kleur_in();
+        System.out.println("Nieuwe kleur van CAR 1: " + userKleur);
+
+        List<Integer> rapairCost = car2.randomNum();
+        
+
+
+
+    }
 }
